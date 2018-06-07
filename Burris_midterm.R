a84c637191782661e8258b5f8841a4bf92b837e1
chrburris <40032919+chrburris@users.noreply.github.com>
2018-06-07 07:53:33 -0700

    Update Burris_midterm.R

--- Burris_midterm.R
+++ Burris_midterm.R
@@ -1,6 +1,21 @@
+################# Christie Burris ##################
+################# Biometry Midterm #################
+
+# Prompt: Dr. Smith happens to like dark chocolate brownies and especially favors those that have the
+# right amount of chewiness and flavor. Suppose that chewiness can be measured (perhaps by
+# eating some). To do this you decide to run an experiment by baking a number of batches of brownies and
+# compare them to come up with the best brownies. There were four different brands that were considered. 
+# In his study he made a batch of each brownies (using 4 8x8 pans, each pan having a different brand mix),
+# set the oven temperature to 350 degrees and placed all four in the oven at once. 
+# This was repeated 10 times on different days. 
+
+# Analyze the data – what questions/hypotheses might you ask prior to experimentation?
+
+##################################################
 # We first run ANOVA as if the design was completely randomized.
 # An ANOVA table is produced from the linear fit of brand and chewiness
-# The results indicate no significant difference in chewiness. However, visualizing the data tells us that the variance amongst each brand is very high.
+# The results indicate no significant difference in chewiness. 
+# However, visualizing the data tells us that the variance amongst each brand is very high.
 # Tukey's multiple comparisons comfirms lack of significance.
 # We instead decide that a randomized block design better describes the experiment.
 
@@ -9,27 +24,29 @@ library(readr)
 Data <- read.csv("~/Desktop/Biometry/brownieCB.csv",header=T)
 attach(Data)
 
-blockF=as.factor(block)
-brandF=as.factor(brand)
+blockF <- as.factor(block)
+brandF <- as.factor(brand)
 
 ######### not taking days to be blocks 
 # (assuming completely randomized design) ##########
 
 # visualize data, lots of variance within brands, less variance between bands
-plot(brand,chewy,ylab="Chewiness", xlab="Brand")
+plot(brand,chewy,ylab = "Chewiness", xlab = "Brand")
 
 # fit linear model
-fit <- lm(chewy ~brandF, data=Data)
+fit <- lm(chewy ~ brandF, data = Data)
 anova(fit)
 
 # check assumptions
 qqnorm(fit$residuals)
+
 # compare to normal
 qqline(fit$residuals) 
 
 # Tukey's multiple comparisons 
 library(multcompView)
 library(emmeans) 
+
 fit_e <- emmeans(fit, "brandF") #estimate means from fit and factor=brand
 pairs(fit_e)  # intervals for pairs
 plot(fit_e)   # plot means and std errors
@@ -40,7 +57,9 @@ cld(fit_e,adjust="Tukey")
 ######### taking days to be blocks ##########
 # A plot of the data by days indicates possible significant difference in chewiness across brands.
 # The ANOVA table indicates significance difference in brand means and day means. 
-# Tukey comparisons indicates brands 1 and 2 are not significantly difference, but brand 4 is significantly different from 1 and 2. Brand 3 is not significantly different from any of the other brands. 
+# Tukey comparisons indicates brands 1 and 2 are not significantly difference, 
+#  but brand 4 is significantly different from 1 and 2. 
+# Brand 3 is not significantly different from any of the other brands. 
 
 # plot data, x=days, y=chewiness, color=brand 
 plot(block,chewy,col=c("red","blue","black","green")[brand],pch = c(15,16,17,18)[brand])
